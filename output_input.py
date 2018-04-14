d88a52fce76e8f162f99c9628d866462d04406f0
luhuajcdd <luhuazhuce@163.com>
2018-04-14 16:38:22 +0800

    output input test

---
+++ output_input.py
@@ -0,0 +1,20 @@
+
+#coding:utf-8
+
+print("test output");
+
+
+test = input()
+# 你好 hello.python
+
+print('input=', test)
+
+
+print('\\t\tewe');
+print(r'\\t\tewe');
+print('''
+	line1,
+	line2
+	line3
+
+	''')
